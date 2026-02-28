b66e929b039a8e000243a594f9648fd7a9df2582
Mac Ens <mac@macens.ca>
2026-02-27 18:37:19 -0600

    More progress

--- scenes/Paddle.cs
+++ scenes/Paddle.cs
@@ -4,54 +4,33 @@ using System.Diagnostics;
 
 public partial class Paddle : CharacterBody2D
 {
-    [Export]
-    public Puck Puck { get; set; }
+	[Export]
+	public Puck Puck { get; set; }
 
-    [Export]
-    public float Speed { get; set; } = 5f;
+	[Export]
+	public float Speed { get; set; } = 250f;
 
+	[Export]
+	public float MaxSpeed { get; set; } = 300f;
 
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
+	}
 
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
+		HandleInput();
+		MoveAndSlide();
+		//HandleCollisions();
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        HandleInput();
-        MoveAndSlide();
-        HandleCollisions();
-    }
-
-    private void HandleInput()
-    {
-        var direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-        Position += direction * Speed;
-        Velocity = Vector2.Zero;
-    }
-
-    private void HandleCollisions()
-    {
-        for (int i = 0; i < GetSlideCollisionCount(); i++)
-        {
-            var collision = GetSlideCollision(i);
-            if (collision.GetCollider() is Puck puck)
-            {
-                OnPuckCollision(puck);
-            }
-        }
-    }
-
-    private void OnPuckCollision(Puck puck)
-    {
-        var direction = (puck.GlobalPosition - GlobalPosition).Normalized();
-        var puckSpeed = Math.Max(puck.Velocity.Length(), 1);
-        var paddleSpeed = Velocity.Length();
-        puckSpeed += paddleSpeed; // Increase puck speed based on paddle speed
-        puck.Velocity = direction * puckSpeed;
-        Debug.WriteLine($"Puck collided with speed {puckSpeed} and direction {direction}");
-    }
+	private void HandleInput()
+	{
+		var direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down").Normalized();
+		Velocity = direction * Speed;
+		
+	}
 }
--- scenes/Puck.cs
+++ scenes/Puck.cs
@@ -4,23 +4,66 @@ using System.Diagnostics;
 
 public partial class Puck : CharacterBody2D
 {
-    public override void _Process(double delta)
-    {
-        for (int i = 0; i < GetSlideCollisionCount(); i++)
-        {
-            var collision = GetSlideCollision(i);
-            if (collision.GetCollider() is StaticBody2D boards)
-            {
-                OnWallCollision(boards);
-            }
-        }
+	public override void _PhysicsProcess(double delta)
+	{
+		MoveAndSlide();
+		HandleCollisions();
+	}
 
-        MoveAndSlide();
-    }
+	private void HandleCollisions()
+	{
+		for (int i = 0; i < GetSlideCollisionCount(); i++)
+		{
+			var collision = GetSlideCollision(i);
+			var collider = collision.GetCollider();
+			
+			if (collider is CharacterBody2D paddle)
+			{
+				OnPaddleCollision(paddle, collision);
+			}
+			else if (collider is StaticBody2D boards)
+			{
+				var puckDirection = Velocity.Normalized();
+				//Debug.WriteLine($"\nPuck collided with boards");
+				//Debug.WriteLine($"Puck speed {Velocity.Length()}");
+				//Debug.WriteLine($"Puck direction {puckDirection}");
+				//Debug.WriteLine($"Collision Position {collision.GetPosition()}");
+				//Debug.WriteLine($"Collision Normal {collision.GetNormal()}");
+				
+				//Debug.WriteLine($"Puck direction {Velocity.Normalized()}");
+				Velocity = Velocity.Bounce(collision.GetNormal());
+				//Debug.WriteLine($"Bounce direction {Velocity.Normalized()}");
+				
+				OnWallCollision(boards);
+			}
+		}
+	}
 
-    private void OnWallCollision(StaticBody2D boards)
-    {
-        var puckDirection = Velocity.Normalized();
-        Debug.WriteLine($"Puck collided with boards at speed {Velocity.Length()} and direction {puckDirection} at {boards.GlobalPosition}");
-    }
+	private void OnWallCollision(StaticBody2D boards)
+	{
+	}
+
+	private void OnPaddleCollision(CharacterBody2D paddle, KinematicCollision2D collision)
+	{
+		Debug.WriteLine($"\nPuck collided with PADDLE");
+		
+		var collisionPosition = collision.GetPosition();		
+		var paddleToCollision = collisionPosition - paddle.GlobalPosition;
+		var puckToCollision = collisionPosition - GlobalPosition; 
+		
+		Debug.WriteLine($"paddleToCollision: {paddleToCollision}");
+		Debug.WriteLine($"puckToCollision: {puckToCollision}");
+		Debug.WriteLine($"Collision Position {collision.GetPosition()}");
+		Debug.WriteLine($"Collision Normal {collision.GetNormal()}");
+		
+		var direction = (GlobalPosition - paddle.GlobalPosition).Normalized();
+		Debug.WriteLine($"direction: {direction * 10}");
+		Debug.WriteLine($"GetNormal: {collision.GetNormal() * 10}");
+		Debug.WriteLine($"Bounce: {Velocity.Bounce(collision.GetNormal()) * 10}");
+		var paddleSpeed = paddle.Velocity.Length();
+		Debug.WriteLine($"paddleSpeed: {paddleSpeed}");
+		var colliderSpeed = collision.GetColliderVelocity().Length();
+		Debug.WriteLine($"colliderSpeed: {colliderSpeed}");
+		Velocity = direction * paddleSpeed;
+	}
 }
